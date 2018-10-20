22cf00cc0982dcd63dc22ea5a8976e246a51a395
Justin Espiritu <Espiritu.L.Justin@gmail.com>
2018-10-19 21:43:27 -0500

    Changed icon, moved app.run to App.js, added more comments

--- Scripts/App.js
+++ Scripts/App.js
@@ -1,1 +1,6 @@
 const app = angular.module("DogFilter", ["ngMask"])
+
+//this will get the breeds on page load before anything controllers and factories are initialized
+app.run(["DogsService", function(dogsService){
+    dogsService.getDogBreeds();
+}])
