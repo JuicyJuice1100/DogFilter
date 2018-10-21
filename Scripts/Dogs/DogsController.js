c21be69a5e897318186231362272af8120be5cf8
Justin Espiritu <Espiritu.L.Justin@gmail.com>
2018-10-21 04:11:14 -0500

    Changed the name of App.js to index.js, fixed issue #7

--- Scripts/Dogs/DogsController.js
+++ Scripts/Dogs/DogsController.js
@@ -1,30 +1,19 @@
-app.controller("DogsController", ["$timeout", "HelperFactory", "DogsFactory", "ImageFactory", "DogsService",
-    function($timeout, helperFactory, dogsFactory, imageFactory, dogsService){
-        var dogs = this;
-
+app.controller("DogsController", ["$scope", "HelperFactory", "DogsFactory", "ImageFactory", "DogsService",
+    function($scope, helperFactory, dogsFactory, imageFactory, dogsService){
         /*********** Factories **********/
-        dogs.dogsFactory = dogsFactory;
-        dogs.imageFactory = imageFactory;
-        dogs.helperFactory = helperFactory;
+        $scope.dogsFactory = dogsFactory;
+        $scope.imageFactory = imageFactory;
+        $scope.helperFactory = helperFactory;
+
+        /*********** Services ***********/
+        $scope.getBreedImages = (breed) => dogsService.getBreedImages(breed);
 
         /*********** Functions **********/
-        dogs.getBreedImages = function(breed){
-            dogs.dogsFactory.search = breed;
-            if(dogsFactory.breeds.some(dog => dog.name.toLowerCase() === breed.toLowerCase())){ //checks to see if the input is valid
-                dogsService.getBreedImages(breed);
-            } else {
-                helperFactory.validation.showInvalidBreedMessage = true;
-                $timeout(function(){
-                    helperFactory.validation.showInvalidBreedMessage = false;
-                }, 5000)
-            }  
-        };
-
-        dogs.reset = function(){
+        $scope.reset = function(){
             helperFactory.resetArray(imageFactory.breedImages);
         };
 
-        dogs.resetSearch = function(){
+        $scope.resetSearch = function(){
             dogsFactory.search = ""
         }
     }
--- Scripts/Dogs/DogsServices.js
+++ Scripts/Dogs/DogsServices.js
@@ -1,54 +1,66 @@
 app.service("DogsService", ["$http", "$timeout", "HelperFactory", "DogsFactory", "ImageFactory",
-    function($http, $timeout, helperFactory, dogsFactory, imageFactory){
-        this.getDogBreeds = function (){
-            helperFactory.loading.isLoading = true;
-            $http.get(helperFactory.urls.breedListUrl)
-                .then(function(result){
-                    helperFactory.resetArray(dogsFactory.breeds); //resets all breeds to a blank array
-                    var id = 0; //this will add ids to array of breeds
-                    result.data.message.forEach(function(breed){ //gets all the breeds from dog.api and adds them to the breeds array
-                        dogsFactory.breeds.push(
-                            {
-                                id: id,
-                                name: breed.toUpperCase(),
-                            }
-                        );
-                        id++; //this will increment id so each breed has a unique id   
-                    });
-                }, function(){ //insert error function here
-                    helperFactory.validation.showErrorMessage = true; //show error message
+    function ($http, $timeout, helperFactory, dogsFactory, imageFactory) {
+        const services = {
+            getDogBreeds: () => {
+                helperFactory.loading.isLoading = true;
+                $http.get(helperFactory.urls.breedListUrl)
+                    .then((result) => {
+                        helperFactory.resetArray(dogsFactory.breeds); //resets all breeds to a blank array
+                        var id = 0; //this will add ids to array of breeds
+                        result.data.message.forEach((breed) =>{ //gets all the breeds from dog.api and adds them to the breeds array
+                            dogsFactory.breeds.push(
+                                {
+                                    id: id,
+                                    name: breed.toUpperCase(),
+                                }
+                            );
+                            id++; //this will increment id so each breed has a unique id   
+                        });
+                    }, () => { //insert error function here
+                        helperFactory.validation.showErrorMessage = true; //show error message
 
-                    $timeout(function(){ //hide error message after 5 seconds
-                        helperFactory.validation.showErrorMessage = false;
-                    }, 5000)
-                }).finally(function(){
-                    helperFactory.loading.isLoading = false; //hide loading gif
-                });
-        };
-        this.getBreedImages = function (breed){
-            helperFactory.loading.isLoading = true;
-            $http.get(helperFactory.urls.breedImagesUrl + breed.toLowerCase() + "/images")
-                .then(function(result){
-                    helperFactory.resetArray(imageFactory.breedImages); //reset all images to a blank array
-                    var id = 0;
-                    result.data.message.forEach(function(image){ //gets all the images of given breed from dog.api and adds them to the image array
-                        imageFactory.breedImages.push(
-                            {
-                                id: id,
-                                link: image
-                            }
-                        );
-                        id++;
+                        $timeout(() => { //hide error message after 5 seconds
+                            helperFactory.validation.showErrorMessage = false;
+                        }, 5000)
+                    }).finally(() => {
+                        helperFactory.loading.isLoading = false; //hide loading gif
                     });
-                }, function(){ //insert error function here
-                    helperFactory.validation.showErrorMessage = true; //show error message
-                    
-                    $timeout(function(){ //hide error message after 5 seconds
-                        helperFactory.validation.showErrorMessage = false;
+            },
+            getBreedImages: (breed) => {
+                dogsFactory.search = breed;
+                if (dogsFactory.breeds.some(dog => dog.name.toLowerCase() === breed.toLowerCase())) { //checks to see if the input is valid
+                    helperFactory.loading.isLoading = true;
+                    $http.get(helperFactory.urls.breedImagesUrl + breed.toLowerCase() + "/images")
+                        .then((result) => {
+                            helperFactory.resetArray(imageFactory.breedImages); //reset all images to a blank array
+                            var id = 0;
+                            result.data.message.forEach((image) => { //gets all the images of given breed from dog.api and adds them to the image array
+                                imageFactory.breedImages.push(
+                                    {
+                                        id: id,
+                                        link: image
+                                    }
+                                );
+                                id++;
+                            });
+                        }, () => { //insert error function here
+                            helperFactory.validation.showErrorMessage = true; //show error message
+
+                            $timeout(() => { //hide error message after 5 seconds
+                                helperFactory.validation.showErrorMessage = false;
+                            }, 5000)
+                        }).finally(() => {
+                            helperFactory.loading.isLoading = false; //hide loading gif
+                        });
+                } else { //if input is not valid then show validation message
+                    helperFactory.validation.showInvalidBreedMessage = true;
+                    $timeout(() => {
+                        helperFactory.validation.showInvalidBreedMessage = false;
                     }, 5000)
-                }).finally(function(){
-                    helperFactory.loading.isLoading = false; //hide loading gif
-                });
-        };
+                }
+
+            }
+        }
+        return services;
     }
 ]);
